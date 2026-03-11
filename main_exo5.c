fefc14bdebd2e5f85a6438ac5d6ba0017b4721a3
Eduardo Guzman Maldonado <eduardo.guzman@innovamics.com>
2026-03-11 09:57:03 +0100

    feat:  solution exo5

--- main_exo5.c
+++ main_exo5.c
@@ -2,39 +2,68 @@
 #include <stdlib.h> // malloc, calloc, realloc, rand, srand
 #include <time.h>  // pour time
 
+// creation avec une procedure
+void creerTab2DV1(int ***pTab, int *pNbLin, int * pNbCol) {
+    printf("Donnez le nb des lignes et des colonnes : ");
+    scanf("%d %d", pNbLin, pNbCol);
+
+    *pTab = calloc(*pNbLin , sizeof(int *));
+
+    for (int i = 0; i < *pNbLin; ++i) {
+        (*pTab)[i] = calloc(*pNbCol, sizeof(int));
+    }
+
+}
+
+// creation avec une fonction
+int** creerTab2DV2(int *pNbLin, int * pNbCol) {
+    int ** tab = NULL;
+
+    printf("Donnez le nb des lignes et des colonnes : ");
+    scanf("%d %d", pNbLin, pNbCol);
+
+    tab = calloc(*pNbLin , sizeof(int *));
+    for (int i = 0; i < *pNbLin; ++i) {
+        tab[i] = calloc(*pNbCol, sizeof(int));
+    }
+
+    return tab;
+}
+
+void randTab(int **tab, const int lin, const int col) {
+    for (int i = 0; i < lin; ++i) {
+        for (int j = 0; j < col; ++j) {
+            // i,j
+            tab[i][j] = rand() % 256;
+        }
+    }
+}
+
+void afficherTab(int **tab, const int lin, const int col) {
+    for (int i = 0; i < lin; ++i) {
+        for (int j = 0; j < col; ++j) {
+            printf("%d ", tab[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     srand(time(NULL));
 
     // variables
-    int ** tab = NULL;
-    int col =0, lin =0;
+    int **tab = NULL;
+    int col = 0, lin = 0;
 
-    // input
-    printf("Donnez le nb des lignes et des colonnes : ");
-    scanf("%d %d", &lin, &col);
-
-    // allocation dynamique
-    tab  =  calloc(lin, sizeof(int *));
-    for (int i = 0; i < lin; ++i) {
-        tab[i] =calloc(col, sizeof(int));
-    }
+    // Creation de matrice
+    creerTab2DV1(&tab, &lin, &col);
+    //tab = creerTab2DV2(&lin, &col); // version 2
 
     // matrice aleatoire
-    for (int i = 0; i < lin; ++i) {
-        for (int j = 0; j < col; ++j) {
-            // i,j
-            tab[i][j] = rand()%256;
-        }
-    }
+    randTab(tab, lin, col);
 
     // afficher
-    for (int i = 0; i < lin; ++i) {
-        for (int j = 0; j < col; ++j) {
-           printf("%d ", tab[i][j]);
-        }
-
-        printf("\n");
-    }
+    afficherTab(tab, lin, col);
 
     // la liberation se fait dans
     // le sense inverse que l'allocation
